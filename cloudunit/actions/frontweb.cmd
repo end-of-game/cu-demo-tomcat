94367b6df0e7dbf03cfb1131f136fd0159699663
Administrator <admin@example.com>
2016-09-16 16:32:02 +0000

    Update frontweb.cmd

--- cloudunit/actions/frontweb.cmd
+++ cloudunit/actions/frontweb.cmd
@@ -1,4 +1,4 @@
-connect --login johndoe --password abc2015 --host http://cuplatform_tomcat_1.tomcat.cloud.unit:8080
+connect --login johndoe --password abc2015 --host http://192.168.50.4:8080
 
 create-app --name front-#APPNAME --type apache-2-2
 
