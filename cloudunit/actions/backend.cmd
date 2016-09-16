c04952e1891a0341fb4c4ba05ef2da6c7e75cd21
Administrator <admin@example.com>
2016-09-16 17:13:25 +0000

    Update backend.cmd

--- cloudunit/actions/backend.cmd
+++ cloudunit/actions/backend.cmd
@@ -1,4 +1,4 @@
-connect --login johndoe --password abc2015 --host http://192.168.50.4:8080
+connect --login johndoe --password abc2015 --host http://192.168.50.1:8080
 
 create-app --name backend-#APPNAME --type tomcat-8
 use backend#APPNAME
