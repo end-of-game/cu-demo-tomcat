bf48dc2f908d9638de84a132f4cc1e80786457cf
Administrator <admin@example.com>
2016-09-21 09:45:50 +0000

    Update backend.cmd

--- cloudunit/actions/backend.cmd
+++ cloudunit/actions/backend.cmd
@@ -1,9 +1,8 @@
 connect --login johndoe --password abc2015 --host http://192.168.50.1:8080
 
-create-app --name backend-#APPNAME --type tomcat-8
+create-app --name backend#APPNAME --type tomcat-8
 use backend#APPNAME
 add-jvm-option "-Dspring.profiles.active=embedded"
 add-module --name mysql-5-5
 
 disconnect
-
